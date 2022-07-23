b7148cea82818ff06fb35ed37827be0e3d009476
Mina Mikhael <mina.s.mikhael@gmail.com>
2022-07-23 20:23:54 +0200

    work in progress

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ import axios from "axios";
 import { URL } from "./constants";
 
 const initialSearch = "";
+const initialSearchText = "coding";
 function App({ loading, error, fetchStart, fetchSuccess, fetchFail }) {
   const [search, setSearch] = useState(initialSearch);
+  const [searchText, setSearchText] = useState(initialSearchText);
 
   const submitHandler = (e) => {
     e.preventDefault();
+    if (search) {
+      setSearchText(search);
+    }
+    setSearch(initialSearch);
   };
   const changeHandler = (e) => {
     setSearch(e.target.value);
@@ -20,12 +26,12 @@ function App({ loading, error, fetchStart, fetchSuccess, fetchFail }) {
   useEffect(() => {
     fetchStart();
     axios
-      .get(URL)
+      .get(`${URL}q=${searchText}`)
       .then((res) => {
         fetchSuccess(res.data.data);
       })
       .catch((err) => fetchFail(err.message));
-  }, []);
+  }, [searchText]);
   return (
     <div className="App">
       <h1>Get random Gifs</h1>
@@ -34,7 +40,8 @@ function App({ loading, error, fetchStart, fetchSuccess, fetchFail }) {
           type="text"
           name="search"
           placeholder="search gif by text"
-          onChange={changeHandler}></input>
+          onChange={changeHandler}
+          value={search}></input>
         &nbsp;
         <button>Search</button>
       </form>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
 /// Constants for the api
 export const URL =
-  "https://api.giphy.com/v1/gifs/trending?api_key=PTEjVQlnoH865SO97d7D1wqBPvJQOKx8&limit=10&rating=g";
+  "https://api.giphy.com/v1/gifs/search?api_key=PTEjVQlnoH865SO97d7D1wqBPvJQOKx8&limit=15&offset=0&rating=g&lang=en&";
 // export const API_KEY = "PTEjVQlnoH865SO97d7D1wqBPvJQOKx8";
+
